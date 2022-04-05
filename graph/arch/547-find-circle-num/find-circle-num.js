1fe917665dbd7cf560a20bc08624f8679377c34c
George <georgevs@gmail.com>
2022-04-05 19:30:54 +0300

    leetcode graph 547 find circle num

---
+++ graph/arch/547-find-circle-num/find-circle-num.js
@@ -0,0 +1,23 @@
+// 1 <= n <= 200
+// n == m.length
+// n == m[i].length
+// m[i][j] is 1 or 0.
+// m[i][i] == 1
+// m[i][j] == m[j][i]
+
+const { union } = require('../disjoint-sets/quick-find-union');
+// const { union } = require('../disjoint-sets/quick-connect-path-compression-union');
+// const { union } = require('../disjoint-sets/quick-connect-rank-path-compression-union');
+
+const findCircleNum = (xs) => {
+  const n = xs.length;
+  const u = union(n);
+  for (let i = 0; i < n; ++i) {
+    for (let j = i + 1; j < n; ++j) {
+      if (xs[i][j] == 1) { u.connect(i, j) }
+    }
+  }
+  return u.numSets();
+};
+
+module.exports = { findCircleNum };
