5ec54c019d54cbc942b8d6848e40de547e97adf9
george <georgevs@gmail.com>
2023-04-19 15:05:31 +0300

    leetcode sql 1251 average selling price

---
+++ sql/arch/1251-average-selling-price/schema.sql
@@ -0,0 +1,6 @@
+drop schema if exists leetcode;
+create schema leetcode;
+use leetcode;
+
+Create table If Not Exists Prices (product_id int, start_date date, end_date date, price int);
+Create table If Not Exists UnitsSold (product_id int, purchase_date date, units int);
